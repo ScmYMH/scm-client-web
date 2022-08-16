c137b18fc1911b2eb41d6d739a694c9abb7e5711
hyeja <21800799@handong.edu>
2022-08-16 14:39:28 +0900

    엑셀 import 완료

--- src/components/tariffInfo/TariffExcelModal.tsx
+++ src/components/tariffInfo/TariffExcelModal.tsx
@@ -1,4 +1,7 @@
-import { useRef } from "react";
+import { postExcelImport } from "api/excelImportAxios";
+import { postTariffExcelImportAsync } from "modules/importExcel/action";
+import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Button, Input, Modal, ModalBody, ModalHeader } from "reactstrap";
 import * as XLSX from "xlsx";
 
@@ -9,6 +12,9 @@ const TariffExcelModal = ({
   isOpen: boolean;
   closeModal: any;
 }) => {
+  let jsonData;
+
+  const [excelData, setExcelData] = useState<any>([]);
   //Excel to Json
   const readUploadFile = (e) => {
     e.preventDefault();
@@ -20,18 +26,28 @@ const TariffExcelModal = ({
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const json = XLSX.utils.sheet_to_json(worksheet);
-        console.log(json);
+        jsonData = json;
+        console.log("파일 json 변환 확인 >>>> ", json);
       };
       reader.readAsArrayBuffer(e.target.files[0]);
-      console.log(reader.readAsArrayBuffer(e.target.files[0]));
     }
+  };
+
+  const dispatch = useDispatch();
+
+  const onSubmitExcelData = () => {
+    console.log("디스패치할 때 json 값 확인 >>> ", jsonData);
+    dispatch(postTariffExcelImportAsync.request(jsonData));
   };
 
   return (
     <Modal isOpen={isOpen} toggle={closeModal} size="m">
       <ModalHeader toggle={closeModal}>
         엑셀 업로드
-        <Button style={{ position: "absolute", right: 0, marginRight: "50px" }}>
+        <Button
+          style={{ position: "absolute", right: 0, marginRight: "50px" }}
+          onClick={onSubmitExcelData}
+        >
           Import
         </Button>
       </ModalHeader>
