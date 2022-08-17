fb0e4560eb8ecd4cf2dd80777fba4fb92f2b4552
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-08-17 17:37:58 +0900

    sidebar update

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -19,38 +19,63 @@ interface SideBarProps {
   toggle: any;
 }
 const SideBar = ({ isOpen, toggle }: SideBarProps) => (
-  <div className={classNames("sidebar", { "is-open": isOpen })}>
+  <div
+    className={classNames("sidebar", { "is-open": isOpen })}
+    style={{ backgroundColor: "#fff" }}
+  >
     <div className="sidebar-header">
-      <span color="green" onClick={toggle} style={{ color: "#fff" }}>
+      <span color="green" onClick={toggle}>
         &times;
       </span>
-      <h3>POSCO ICT</h3>
+      <h3 style={{ color: "#black" }}>POSCO ICT</h3>
     </div>
     <div className="side-menu">
-      <Nav vertical className="list-unstyled pb-3">
+      <Nav
+        vertical
+        justified
+        pills
+        fill
+        className="list-unstyled pb-3 justified"
+      >
         <p>CONTENT</p>
         <NavItem>
-          <NavLink style={{ color: "#fff" }} tag={Link} to={"/contractMember"}>
-            {/* <FontAwesomeIcon icon={faImage} className="mr-2" /> */}
-            계약 담당자 등록
+          <NavLink
+            style={{ color: "black", marginBottom: "70px" }}
+            tag={Link}
+            to={"/"}
+          >
+            {/* <FontAwesomeIcon icon={faBriefcase} className="mr-2" /> */}
+            운송 계약 등록
           </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink style={{ color: "#fff" }} tag={Link} to={"/changeManager"}>
+          <NavLink
+            style={{ color: "black", marginBottom: "70px" }}
+            tag={Link}
+            to={"/changeManager"}
+          >
             {/* <FontAwesomeIcon icon={faQuestion} className="mr-2" /> */}
             계약 담당자 변경
           </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink style={{ color: "#fff" }} tag={Link} to={"/"}>
-            {/* <FontAwesomeIcon icon={faBriefcase} className="mr-2" /> */}
-            수출 COA
+          <NavLink
+            style={{ color: "black", marginBottom: "70px" }}
+            tag={Link}
+            to={"/contractMember"}
+          >
+            {/* <FontAwesomeIcon icon={faImage} className="mr-2" /> */}
+            계약 담당자 등록
           </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink style={{ color: "#fff" }} tag={Link} to={"/tariff"}>
+          <NavLink
+            style={{ color: "black", marginBottom: "100px" }}
+            tag={Link}
+            to={"/tariff"}
+          >
             {/* <FontAwesomeIcon icon={faPaperPlane} className="mr-2" /> */}
-            Tariff
+            정산
           </NavLink>
         </NavItem>
       </Nav>
@@ -58,30 +83,4 @@ const SideBar = ({ isOpen, toggle }: SideBarProps) => (
   </div>
 );
 
-const submenus = [
-  [
-    {
-      title: "Home 1",
-      target: "Home-1",
-    },
-    {
-      title: "Home 2",
-      target: "Home-2",
-    },
-    {
-      itle: "Home 3",
-      target: "Home-3",
-    },
-  ],
-  [
-    {
-      title: "Page 1",
-      target: "Page-1",
-    },
-    {
-      title: "Page 2",
-      target: "Page-2",
-    },
-  ],
-];
 export default SideBar;
