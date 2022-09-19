821d0fa17161cfb7352ecc0b54ec58eeac866d96
Mino94 <mh.kim368@gmail.com>
2022-09-19 11:59:25 +0900

    shipmentinfo form add

---
+++ src/components/shipmentInfo/ShipMentInfoForm.tsx
@@ -0,0 +1,14 @@
+import { Card } from "reactstrap";
+
+const ShipMentInfoForm = () => {
+    return (
+    <>
+    <div>
+        <h1>구글 맵</h1>
+      </div>
+    </>
+    )
+}
+
+
+export default ShipMentInfoForm;
