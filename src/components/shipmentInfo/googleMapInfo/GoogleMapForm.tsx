fedee21a9b51796955a320cacf6dc1b147285372
Mino94 <mh.kim368@gmail.com>
2022-09-22 10:07:35 +0900

    polyline 그리기

--- src/components/shipmentInfo/googleMapInfo/GoogleMapForm.tsx
+++ src/components/shipmentInfo/googleMapInfo/GoogleMapForm.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-// import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from 'google-map-react';
 import { Polyline, GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
@@ -13,18 +13,20 @@ const center = {
 };
 
 const GoogleMapForm = () => {
-    const cordinates = { lat:34.9328653, lng:127.7361051};
+    const cordinates = { lat: 34.9328653, lng: 127.7361051};
     const mapRef = useRef();
     const path = [
-        {lat: 37.772, lng: -122.214},
-        {lat: 21.291, lng: -157.821},
-        {lat: -18.142, lng: 178.431},
-        {lat: -27.467, lng: 153.027}
+        {lat: 34.9328653, lng: 127.7361051},
+        {lat: 31.090396564020192,  lng: 128.64815478753206},
+        {lat: 21.04961147500605,  lng: 154.03320910099987},
+        {lat: 25.503442799160354,  lng: -159.09579921666798}, 
+        {lat: 34.07364171303651, lng:  -118.23872946479459}
     ];
 
     const onLoad = polyline => {
         console.log('polyline: ', polyline)
     };
+    
     const options = {
         strokeColor: '#FF0000',
         strokeOpacity: 0.8,
@@ -49,17 +51,7 @@ const GoogleMapForm = () => {
         width: "800px"
       };
     return (
-        // <GoogleMapReact
-        //     bootstrapURLKeys={{key: 'AIzaSyC54rGP0q3aIP_wiWu9U8GJ8trQUbr7cqk'}}
-        //     defaultCenter={cordinates}
-        //     center={cordinates}
-        //     defaultZoom={3}
-        //     margin = {[50, 50, 50, 50]}
-        //     options={''}
-        //     onChange={''}
-        //     onChildClick={''}
-        // >
-        // </GoogleMapReact>
+        
         <LoadScript
              googleMapsApiKey="AIzaSyC54rGP0q3aIP_wiWu9U8GJ8trQUbr7cqk"
          >
@@ -75,6 +67,7 @@ const GoogleMapForm = () => {
                 path={path}
                 options={options}
             />
+           
             </GoogleMap>
         </LoadScript>
         // <LoadScript
